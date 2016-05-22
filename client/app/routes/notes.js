2f25addd6e3ffbcd32160b9a9d304c34dd8fab2b
Scott Faria <scott.faria@gmail.com>
2016-05-22 14:31:26 +0200

    Some notes rendering

--- client/app/routes/notes.js
+++ client/app/routes/notes.js
@@ -1,5 +1,28 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
+const { get } = Ember;
+
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+
+    errorService: Ember.inject.service('error'),
+
+    model() {
+        return new Ember.RSVP.Promise((resolve, reject) => {
+            Ember.$.ajax({
+                type: 'GET',
+                url: '/notes'
+            }).done(result => {
+                resolve(result);
+            }).fail(error => {
+                get(this, 'errorService').setError(error);
+                reject();
+            });
+        });
+    },
+
+    setupController(controller, model) {
+        controller.set('notes', model);
+    }
+
 });
